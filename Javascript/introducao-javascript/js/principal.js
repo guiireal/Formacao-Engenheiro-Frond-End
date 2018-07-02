57bff5a116616bba2db277c933c4689a83145041
Guilherme França <guii_real@hotmail.com>
2018-07-02 00:29:18 -0300

    Adicionando regras para validação de peso e altura

--- Javascript/introducao-javascript/js/principal.js
+++ Javascript/introducao-javascript/js/principal.js
@@ -1,2 +1,27 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
