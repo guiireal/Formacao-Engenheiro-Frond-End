bfda51050dcf55386b2a1e63d7ccf684870aa38b
Guilherme França <guii_real@hotmail.com>
2018-08-26 00:20:38 -0300

    Bônus e Certificado

--- JS_Avancado_2/aluraframe/client/js/app/polyfill/es6.js
+++ JS_Avancado_2/aluraframe/client/js/app/polyfill/es6.js
@@ -1,4 +1,4 @@
-if(!Array.prototype.includes) {
+if (!Array.prototype.includes) {
     console.log('Polyfill para Array.includes aplicado.');
     Array.prototype.includes = function(elemento) {
         return this.indexOf(elemento) != -1;
